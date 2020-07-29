74427a710548b36ff5318c01a9694eb8ce2902fd
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-07-29 09:31:09 -0700

    Added ApiContext.js, added context to App.js, NoteListNav.js, NotePageNav.js

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
 import NoteListMain from '../NoteListMain/NoteListMain';
 import NotePageMain from '../NotePageMain/NotePageMain';
+import ApiContext from '../ApiContext';
 import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
 import './App.css';
 
@@ -32,8 +33,14 @@ class App extends Component {
             .catch(e => console.log(e.message))
     }
 
+    deleteNote = noteId => {
+        const newNotes = this.state.notes.filter(note => note.id !== noteId);
+        this.setState({
+            notes: newNotes
+        })
+    }
+
     renderNavRoutes() {
-        const {notes, folders} = this.state;
         return (
             <>
                 {['/', '/folder/:folderId'].map(path => (
@@ -41,23 +48,12 @@ class App extends Component {
                         exact
                         key={path}
                         path={path}
-                        render={routeProps => (
-                            <NoteListNav
-                                folders={folders}
-                                notes={notes}
-                                {...routeProps}
-                            />
-                        )}
+                        component={NoteListNav}
                     />
                 ))}
                 <Route
                     path="/note/:noteId"
-                    render={routeProps => {
-                        const {noteId} = routeProps.match.params;
-                        const note = findNote(notes, noteId) || {};
-                        const folder = findFolder(folders, note.folderId);
-                        return <NotePageNav {...routeProps} folder={folder} />;
-                    }}
+                    component={NotePageNav}
                 />
                 <Route path="/add-folder" component={NotePageNav} />
                 <Route path="/add-note" component={NotePageNav} />
@@ -66,7 +62,6 @@ class App extends Component {
     }
 
     renderMainRoutes() {
-        const {notes, folders} = this.state;
         return (
             <>
                 {['/', '/folder/:folderId'].map(path => (
@@ -74,45 +69,34 @@ class App extends Component {
                         exact
                         key={path}
                         path={path}
-                        render={routeProps => {
-                            const {folderId} = routeProps.match.params;
-                            const notesForFolder = getNotesForFolder(
-                                notes,
-                                folderId
-                            );
-                            return (
-                                <NoteListMain
-                                    {...routeProps}
-                                    notes={notesForFolder}
-                                />
-                            );
-                        }}
+                        component={NoteListMain}
                     />
                 ))}
                 <Route
                     path="/note/:noteId"
-                    render={routeProps => {
-                        const {noteId} = routeProps.match.params;
-                        const note = findNote(notes, noteId);
-                        return <NotePageMain {...routeProps} note={note} />;
-                    }}
+                    component={NotePageMain}
                 />
             </>
         );
     }
 
     render() {
+        const contextValue ={
+            deleteNote: this.deleteNote
+        }
         return (
-            <div className="App">
-                <nav className="App__nav">{this.renderNavRoutes()}</nav>
-                <header className="App__header">
-                    <h1>
-                        <Link to="/">Noteful</Link>{' '}
-                        <FontAwesomeIcon icon="check-double" />
-                    </h1>
-                </header>
-                <main className="App__main">{this.renderMainRoutes()}</main>
-            </div>
+            <ApiContext.Provider value={contextValue}>
+                <div className="App">
+                    <nav className="App__nav">{this.renderNavRoutes()}</nav>
+                    <header className="App__header">
+                        <h1>
+                            <Link to="/">Noteful</Link>{' '}
+                            <FontAwesomeIcon icon="check-double" />
+                        </h1>
+                    </header>
+                    <main className="App__main">{this.renderMainRoutes()}</main>
+                </div>
+            </ApiContext.Provider>
         );
     }
 }
--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
 import './NotePageNav.css'
+import ApiContext from '../ApiContext'
+import { findFolder, findNote } from '../notes-helpers'
 
-export default function NotePageNav(props) {
-  return (
-    <div className='NotePageNav'>
-      <CircleButton
-        tag='button'
-        role='link'
-        onClick={() => props.history.goBack()}
-        className='NotePageNav__back-button'
-      >
-        <FontAwesomeIcon icon='chevron-left' />
-        <br />
-        Back
-      </CircleButton>
-      {props.folder && (
-        <h3 className='NotePageNav__folder-name'>
-          {props.folder.name}
-        </h3>
-      )}
-    </div>
-  )
-}
-
-NotePageNav.defaultProps = {
-  history: {
-    goBack: () => {}
+export default class NotePageNav extends Component {
+  static contextType = ApiContext;
+  static defaultProps = {
+    match: {
+      params: {}
+    },
+    history: {
+      goBack: () => {}
+    }
+  }
+  render() {
+    const {folders = [], notes = []} = this.context;
+    const {noteId} = this.props.match.params;
+    const note = findNote(notes, noteId) || {};
+    const folder = findFolder(folders, note.folderId);
+    return (
+      <div className='NotePageNav'>
+        <CircleButton
+          tag='button'
+          role='link'
+          onClick={() => this.props.history.goBack()}
+          className='NotePageNav__back-button'
+        >
+          <FontAwesomeIcon icon='chevron-left' />
+          <br />
+          Back
+        </CircleButton>
+        {folder && (
+          <h3 className='NotePageNav__folder-name'>
+            {folder.name}
+          </h3>
+        )}
+      </div>
+    )
   }
 }
